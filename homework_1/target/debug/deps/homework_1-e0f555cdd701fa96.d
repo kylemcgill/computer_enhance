2b60160496615480c7601bc1b7feb357cc679dd7
Kyle McGill <kylemcgill@Kyles-MacBook-Pro.local>
2025-06-06 12:51:44 -0700

    cleanup of compiled files

--- homework_1/target/debug/deps/homework_1-e0f555cdd701fa96.d
+++
@@ -1,5 +0,0 @@
-/Users/kylemcgill/workspace/computer_enhance/homework_1/target/debug/deps/libhomework_1-e0f555cdd701fa96.rmeta: src/main.rs
-
-/Users/kylemcgill/workspace/computer_enhance/homework_1/target/debug/deps/homework_1-e0f555cdd701fa96.d: src/main.rs
-
-src/main.rs:
--- homework_1/target/debug/homework_1.d
+++
@@ -1,1 +0,0 @@
-/Users/kylemcgill/workspace/computer_enhance/homework_1/target/debug/homework_1: /Users/kylemcgill/workspace/computer_enhance/homework_1/src/main.rs
